65c4343aac01329432f0274d6e7d60eaecbfd631
ludoff <ludovicofiore25@gmail.com>
2024-06-07 16:17:29 +0200

    build: add project files and first array in email ex

---
+++ esercizio mail/js/main.js
@@ -0,0 +1,23 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+
+// creazione array con email invitati
+const mailList = ["luca@mail", "pippo@mail", "marco@mail", "simone@mail", "andrea@mail"];
+
+console.log(mailList);
+
+
+// input per inserimento email utente
+let userMail = prompt("inserisci la tua mail");
+
+console.log(userMail);
+
+
+// inserimento if 
+
+
+    // se email presente messaggio positivo
+
+
+    // altrimenti messaggio negativo
+
