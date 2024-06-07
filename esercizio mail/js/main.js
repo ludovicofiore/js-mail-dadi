d8552a7d1d80a0378386e10c30f8d497163b1c2b
ludoff <ludovicofiore25@gmail.com>
2024-06-07 16:49:48 +0200

    feat: add cicle and if conditions

--- esercizio mail/js/main.js
+++ esercizio mail/js/main.js
@@ -4,20 +4,44 @@
 // creazione array con email invitati
 const mailList = ["luca@mail", "pippo@mail", "marco@mail", "simone@mail", "andrea@mail"];
 
-console.log(mailList);
-
 
 // input per inserimento email utente
 let userMail = prompt("inserisci la tua mail");
 
-console.log(userMail);
 
+// aggiunta ciclo valori array
 
-// inserimento if 
+for (let i = 0; i < mailList.length; i++) {
 
+    // console.log(mailList[i]);
+    let mailListValue = mailList[i];
+
+    // inserimento if 
 
     // se email presente messaggio positivo
+    if (userMail === mailListValue) {
+        
+        let result = "ammesso";
+
+        document.getElementById("stato").innerHTML = result
+
+        console.log(result);
+
+        // altrimenti messaggio negativo
+    } else {
+
+        let result = "non ammesso";
+
+        document.getElementById("stato").innerHTML = result
+
+        console.log(result);
+    } 
+
+}
 
 
-    // altrimenti messaggio negativo
+
+
+
+
 
