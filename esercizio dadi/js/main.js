51c0c8768dc96a13587597a317ae179947daf956
ludoff <ludovicofiore25@gmail.com>
2024-06-07 18:30:48 +0200

    feat: add random numbers in dice ex

---
+++ esercizio dadi/js/main.js
@@ -0,0 +1,12 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// genero numero casuale per utente
+let randomUser = Math.floor(Math.random() * 6) + 1;
+
+console.log(randomUser);
+
+
+// genero numero casuale per pc
+let randomPc = Math.floor(Math.random() * 6) + 1;
+
+console.log(randomPc);
