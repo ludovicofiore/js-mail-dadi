f31b686c5a7e0aeea3800879e3f795a3920a4816
ludoff <ludovicofiore25@gmail.com>
2024-06-08 12:52:21 +0200

    feat: add output in page and click event

--- esercizio dadi/js/main.js
+++ esercizio dadi/js/main.js
@@ -1,28 +1,67 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// genero numero casuale per utente
-let randomUser = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomUser);
-
-
-// genero numero casuale per pc
-let randomPc = Math.floor(Math.random() * 6) + 1;
-
-console.log(randomPc);
+// dichiaro variabile container
+const divContainer = document.querySelector(".container");
 
 
-// aggiunta condizioni if
+// gestione attività del bottone al click
 
-if (randomUser > randomPc) {
+const myButton = document.querySelector(".go-button");
 
-    console.log("utente vince");
+myButton.addEventListener("click",
+    function() {
 
-} else if (randomUser === randomPc) {
+        // genero numero casuale per utente
+        let randomUser = Math.floor(Math.random() * 6) + 1;
 
-    console.log("ha vinto l'amicizia");
-    
-} else {
+        console.log(randomUser);
 
-    console.log("PC vince");
-}
+
+        // genero numero casuale per pc
+        let randomPc = Math.floor(Math.random() * 6) + 1;
+
+        console.log(randomPc);
+
+
+        // aggiunta condizioni if
+
+            // se numero utente maggiore numero pc
+        if (randomUser > randomPc) {
+
+            let result = "Utente vince"
+
+            // creo elemento e porto risultato in pagina
+            const element = `<h2>${result}</h2>`;
+            divContainer.innerHTML += element;
+
+            console.log("utente vince");
+
+            // se numero utente uguale numero pc
+        } else if (randomUser === randomPc) {
+
+            let result = "Ha vinto l'amicizia"
+
+            // creo elemento e porto risultato in pagina
+            const element = `<h2>${result}</h2>`;
+            divContainer.innerHTML += element;
+
+            console.log("ha vinto l'amicizia");
+        
+            // se numero utente minore numero pc
+        } else {
+
+            let result = "PC vince"
+
+            // creo elemento e porto risultato in pagina
+            const element = `<h2>${result}</h2>`;
+            divContainer.innerHTML += element;
+
+            console.log("PC vince");
+        }
+
+
+
+    }
+)
+
